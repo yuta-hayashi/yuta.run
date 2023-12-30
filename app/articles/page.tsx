b58fff2ca7f47e43b28a99fef1112114c6fc2736
yuta-hayashi <dev.hyuta@gmail.com>
2023-12-30 18:30:07 +0900

    article一覧ページを追加

---
+++ app/articles/page.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import { getArticles } from '@/lib/newt'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'yuta.run',
+  description: "yuta's page",
+}
+
+export default async function Page() {
+  const articles = await getArticles()
+  return (
+    <div>
+      <h1>Articles</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.slug}>
+            <Link href={`/articles/${article.slug}`}>{article.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
